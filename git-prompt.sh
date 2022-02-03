212c8f1b438196d149ed803104e84f81c71f5623
Felipe Augusto Janotte <f.a.janotte@gmail.com>
2022-02-03 15:12:45 -0300

    Update git-prompt.sh

--- git-prompt.sh
+++ git-prompt.sh
@@ -9,13 +9,19 @@ if test -f ~/.config/git/git-prompt.sh
 then
 	. ~/.config/git/git-prompt.sh
 else
-	PS1='\[\033]0;Git-Bash: ${PWD//[^[:ascii:]]/?}\007\]' 	# set window title
-	PS1="$PS1"'\n'                 													# new line
-	PS1="$PS1"'\033[36m\u'       														# change color to cyan + User
-	PS1="$PS1"'\033[1;37m in'	      												# change to gray + 'in'
-	# PS1="$PS1"'$MSYSTEM '          												# show MSYSTEM
-	PS1="$PS1"'\033[32m ../\W'       												# change to magenta + current directory
-	# PS1="$PS1"'\d \@ '																		# show the date (week/month/day)
+	PS1='\[\033]0;Git-Bash: ${PWD//[^[:ascii:]]/?}\007\]' 					
+	# set window title
+	
+	PS1="$PS1"'\n'                 												
+	# new line
+	PS1="$PS1"'\033[1;36m\u '       														
+	# change font to cyan + show CurrentUser 
+	
+	PS1="$PS1"'\033[4;37min'	      													
+	# change font to gray + show 'in'
+	
+	PS1="$PS1"'\033[0;32m ../\W'       												
+	# change font to purple + show CurrentDirectory
 	if test -z "$WINELOADERNOEXEC"
 	then
 		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -26,23 +32,86 @@ else
 		then
 			. "$COMPLETION_PATH/git-completion.bash"
 			. "$COMPLETION_PATH/git-prompt.sh"
-			PS1="$PS1"'\033[1;35m `__git_ps1`'  														# change color to magenta bold + bash function
+			PS1="$PS1"'\033[1;36m `__git_ps1`'  														
+			# change font to magenta bold + bash function
 		fi
 	fi
-	PS1="$PS1"'\033[m'        																					# change color to default
-	PS1="$PS1"'\n'                 																			# new line
-	PS1="$PS1"'❱ '                 																			# prompt: always ❱
+	PS1="$PS1"'\033[m'        															
+	# change font to default
+	PS1="$PS1"'\n'																			
+	# new line
+	PS1="$PS1"'\v '																		
+	# show bash version
+	PS1="$PS1"'➤  '                 													
+	# prompt: ➤
 fi
 
-MSYS2_PS1="$PS1"               																				# for detection by MSYS2 SDK's bash.basrc
+MSYS2_PS1="$PS1"               														
+# for detection by MSYS2 SDK's bash.basrc
 
-# Evaluate all user-specific Bash completion scripts (if any)
+# evaluate all user-specific Bash completion scripts (if any)
 if test -z "$WINELOADERNOEXEC"
 then
 	for c in "$HOME"/bash_completion.d/*.bash
 	do
-		# Handle absence of any scripts (or the folder) gracefully
+		# handle absence of any scripts (or the folder) gracefully
 		test ! -f "$c" ||
 		. "$c"
 	done
 fi
+
+# MORE
+
+# * PS1="$PS1"'\n'
+	# ? new line
+
+# * PS1="$PS1"'\d'																	
+	# ? show the date (week/month/day)
+
+# * PS1="$PS1"'\@ '																		
+	# ? show the time (12-hour am/pm)
+
+# * PS1="$PS1"'\A '
+	# ? show the time (24-hour HH:MM)
+
+# * PS1="$PS1"'\t '
+	# ? show the time (24-hour HH:MM:SS)
+
+# * PS1="$PS1"'\T '
+	# ? show the time (12-hour HH:MM:SS)
+
+# * PS1="$PS1"'\033[m'
+	# ? change color
+		# ? \033[30m' - BLACK
+		# ? \033[31m' - RED
+		# ? \033[32m' - GREEN
+		# ? \033[33m' - BROWN
+		# ? \033[34m' - BROWN
+		# ? \033[35m' - PURPLE
+		# ? \033[36m' - CYAN
+		# ? \033[37m' - WHITE
+
+# * PS1="$PS1"'\u '
+	# ? show current username
+
+# * PS1="$PS1"'\h '
+	# ? show the hostname
+# * PS1="$PS1"'\H '
+	# ? show fully domain name
+ 
+# * PS1="$PS1"'\W '
+	# ? show current working directory
+
+# * PS1="$PS1"'\v '
+	# ? show bash version
+
+# * PS1="$PS1"'\V '
+	# ? show bash version + patch
+
+# * PS1="$PS1"'\! '
+	# ? show history number of command
+
+# * PS1="$PS1"'\# '
+	# ? show command number
+
+
