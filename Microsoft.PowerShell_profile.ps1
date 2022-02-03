18ea39d2169e57c1e32f7d4b2619b2f8fea6cb65
Felipe Augusto Janotte <f.a.janotte@gmail.com>
2022-02-03 15:08:15 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,16 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Punk
+
+if (Test-Path "C:\Users\Suporte\.jabba\jabba.ps1") { . "C:\Users\Suporte\.jabba\jabba.ps1" }
+# Jabba config - JVM
+
+$currentUser= [Security.Principal.WindowsIdentity]::GetCurrent().name;
+$currentFolder= Split-Path -Path $pwd -Leaf
+$psVersionMajor= $host.Version.Major # PowerShell Version
+$psVersionMinor= $host.Version.Minor # PowerShell Version Patch
+
+# Write-host "$currentUser in ../$currentFolder"
+Write-host $currentUser -ForegroundColor Cyan -NoNewline
+Write-host " in " -ForegroundColor White -NoNewline
+Write-host "../$currentFolder" -ForegroundColor Green
+
+function prompt { "$psVersionMajor.$psVersionMinor > "}
+
