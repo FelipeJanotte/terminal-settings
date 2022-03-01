8100874ce59dc75bf9d7234591944edede2e63c4
Felipe Augusto Janotte <f.a.janotte@gmail.com>
2022-03-01 12:03:10 -0300

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,10 +7,11 @@ $currentFolder= Split-Path -Path $pwd -Leaf
 $psVersionMajor= $host.Version.Major # PowerShell Version
 $psVersionMinor= $host.Version.Minor # PowerShell Version Patch
 
-# Show "$currentUser in ../$currentFolder"
-Write-host $currentUser -ForegroundColor Cyan -NoNewline
-Write-host " in " -ForegroundColor White -NoNewline
-Write-host "../$currentFolder" -ForegroundColor Green
-
-function prompt { "$psVersionMajor.$psVersionMinor > "}
+function prompt { 
+  # Show "$currentUser in ../$currentFolder"
+  Write-host $currentUser -ForegroundColor Cyan -NoNewline
+  Write-host " in " -ForegroundColor White -NoNewline
+  Write-host "../$currentFolder" -ForegroundColor Green
+  "$psVersionMajor.$psVersionMinor > "
+  }
 # Promp: $VERSION + " > "
